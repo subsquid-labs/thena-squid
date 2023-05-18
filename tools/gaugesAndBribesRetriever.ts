8549120a3c6ef08de3e47154f256e750c47a5d52
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-05-18 17:02:28 +0500

    preindex gauges and bribes

--- tools/gaugesAndBribesRetriever.ts
+++ tools/gaugesAndBribesRetriever.ts
@@ -2,9 +2,8 @@ import assert from 'assert'
 import {lookupArchive} from '@subsquid/archive-registry'
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import {Database, LocalDest} from '@subsquid/file-store'
-import * as algebraPoolFactoryAbi from '../src/abi/algebraFactory'
-import * as solidlyPoolFactoryAbi from '../src/abi/solidlyFactory'
-import {ALGEBRA_FACTORY, SOLIDLY_FACTORY} from '../src/config'
+import * as voterAbi from '../src/abi/voterV3'
+import {ALGEBRA_FACTORY, SOLIDLY_FACTORY, VOTER} from '../src/config'
 
 const earliestPairFactoryDeploymentBlock = 24468802
 
@@ -26,21 +25,17 @@ const processor = new EvmBatchProcessor()
         },
     })
     .addLog({
-        address: [SOLIDLY_FACTORY],
-        topic0: [solidlyPoolFactoryAbi.events.PairCreated.topic],
-    })
-    .addLog({
-        address: [ALGEBRA_FACTORY],
-        topic0: [algebraPoolFactoryAbi.events.Pool.topic],
+        address: [VOTER],
+        topic0: [voterAbi.events.GaugeCreated.topic],
     })
 
-let solidlyPools: string[] = []
-let algebraPools: string[] = []
+let gauges: string[] = []
+let bribes: string[] = []
 
 type Metadata = {
     height: number
     hash: string
-    addresses: Record<string, string[]>
+    addresses: Record<'gauges' | 'bribes', string[]>
 }
 
 let isReady = false
@@ -52,11 +47,11 @@ let db = new Database({
     syncIntervalBlocks: 1,
     hooks: {
         async onStateRead(dest) {
-            if (await dest.exists('pools.json')) {
-                let {height, hash, addresses}: Metadata = await dest.readFile('pools.json').then(JSON.parse)
+            if (await dest.exists('gaugesAndBribes.json')) {
+                let {height, hash, addresses}: Metadata = await dest.readFile('gaugesAndBribes.json').then(JSON.parse)
 
-                solidlyPools.push(...addresses[SOLIDLY_FACTORY])
-                algebraPools.push(...addresses[ALGEBRA_FACTORY])
+                gauges = addresses.gauges
+                bribes = addresses.bribes
 
                 return {height, hash}
             } else {
@@ -67,11 +62,11 @@ let db = new Database({
             let metadata: Metadata = {
                 ...info,
                 addresses: {
-                    [ALGEBRA_FACTORY]: algebraPools,
-                    [SOLIDLY_FACTORY]: solidlyPools,
+                    gauges,
+                    bribes,
                 },
             }
-            await dest.writeFile('pools.json', JSON.stringify(metadata))
+            await dest.writeFile('gaugesAndBribes.json', JSON.stringify(metadata))
 
             isReady = true
         },
@@ -83,15 +78,9 @@ processor.run(db, async (ctx) => {
 
     for (let c of ctx.blocks) {
         for (let i of c.logs) {
-            if (i.address === SOLIDLY_FACTORY) {
-                let {pair} = solidlyPoolFactoryAbi.events.PairCreated.decode(i)
-                solidlyPools.push(pair.toLowerCase())
-            }
-
-            if (i.address === ALGEBRA_FACTORY) {
-                let {pool} = algebraPoolFactoryAbi.events.Pool.decode(i)
-                algebraPools.push(pool.toLowerCase())
-            }
+            let {gauge, external_bribe, internal_bribe} = voterAbi.events.GaugeCreated.decode(i)
+            gauges.push(gauge.toLowerCase())
+            bribes.push(external_bribe.toLowerCase(), internal_bribe.toLowerCase())
         }
     }
 })
